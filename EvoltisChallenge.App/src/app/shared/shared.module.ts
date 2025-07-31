3a9b2913af120985d6087f9cafb4c38f70ba4781
Adam <67810957+tolosaadam@users.noreply.github.com>
2025-07-31 18:01:07 -0300

    feat (spinner-component) : refactor, y utilizo spinner de PrimeNG

--- EvoltisChallenge.App/src/app/shared/shared.module.ts
+++ EvoltisChallenge.App/src/app/shared/shared.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 
@@ -8,7 +12,13 @@ import { LoadingSpinnerComponent } from './components/loading-spinner/loading-sp
         NotFoundComponent, 
         LoadingSpinnerComponent
     ],
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        RouterModule,
+        ProgressSpinnerModule,
+        ButtonModule,
+        CardModule
+    ],
     exports: [
         NotFoundComponent, 
         LoadingSpinnerComponent,

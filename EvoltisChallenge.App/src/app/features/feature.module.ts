0e5f60716e0c4314c8de45d4d016bd7281f684e4
Adam <67810957+tolosaadam@users.noreply.github.com>
2025-07-30 22:31:59 -0300

    feat () : soluciono la navegación.

--- EvoltisChallenge.App/src/app/features/feature.module.ts
+++ EvoltisChallenge.App/src/app/features/feature.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
-import { ProductModule } from './products/product.module';
 import { ProductCategoryModule } from './product-categories/product-category.module';
 
 @NgModule({
     imports: [
-        ProductModule,
         ProductCategoryModule
     ],
     exports: []
